ee0355d7c55effdf13b13cfd4d49f3a32c07c2f4
phynos <phynos@126.com>
2019-07-23 11:20:29 +0800

    up

---
+++ src/pages/topo/Fullscreen.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="topo-fullscreen"></div>
+</template>
+
+<script>
+export default {
+    name: 'topo-fullscreen',
+    methods: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+
+}
+</script>
+
+<style lang="scss">
+    .topo-fullscreen {
+        height: 100vh;
+        width: 100%;
+    }
+</style>
+
+
