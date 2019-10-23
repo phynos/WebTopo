ae0bc7517ed9d5a04783292939f69fcfbf28d56e
phynos <phynos@126.com>
2019-10-23 17:58:56 +0800

    1

--- src/boot/EventBus.js
+++ src/boot/EventBus.js
@@ -1,7 +1,7 @@
 
-import EventBus from 'src/assets/libs/simpleEventBus.js'
+import simpleEventBus from 'src/assets/libs/simpleEventBus.js'
 
 
 export default ({ app, router, store, Vue }) => {
-    Vue.prototype.SimpleEventBus = EventBus;
+    Vue.prototype.SimpleEventBus = simpleEventBus;
   }
