406f3b62eb9c6c8d1463f148be24321f57ae47f9
phynos <phynos@126.com>
2019-12-10 20:48:13 +0800

    增加演示版本的连接线箭头

--- src/components/topo/control/canvas/ViewLineArrow.vue
+++ src/components/topo/control/canvas/ViewLineArrow.vue
@@ -1,7 +1,19 @@
 <template>
-<canvas ref="elCanvas" :width="detail.style.position.w" :height="detail.style.position.h">
-Your browser does not support the HTML5 canvas tag.
-</canvas>
+<div class="view-line-arrow" @mousemove.stop="onMousemove($event)" @mouseup="onMouseUp($event)">
+    <canvas ref="elCanvas" :width="detail.style.position.w" :height="detail.style.position.h">
+        Your browser does not support the HTML5 canvas tag.
+    </canvas>
+
+    <template v-for="(pass,index) in passBy">
+        <div :key="index" class="passby" @mousedown.stop="aroowPassDown(pass,$event,index)" :style="{
+                            left: pass.x + 'px',
+                            top: pass.y + 'px',                           
+                        }">
+
+        </div>
+    </template>
+
+</div>
 </template>
 
 <script>
@@ -12,12 +24,16 @@ export default {
     extends: canvasView,
     data() {
         return {
+            lineWidth: 2,
+            flag: false,
+            passItem: {},
+            passBy: [], //控制点（包含起始和终点）
             FACTOR_H: 5, //箭头 水平高度倍数
             FACTOR_V: 4, //箭头 垂直长度倍数
         }
     },
     methods: {
-        drawArrow(ctx, x1, y1, x2, y2,lineWidth, color) { // (x1, y1)是线段起点  (x2, y2)是线段终点
+        drawArrow(ctx, x2, y2, lineWidth, color) { // (x1, y1)是线段起点  (x2, y2)是线段终点
             ctx.beginPath(); // 坐标原点 => (x2, y2)   
             ctx.moveTo(x2, y2);
             ctx.lineTo(x2 - lineWidth * this.FACTOR_H, y2 - lineWidth * this.FACTOR_V);
@@ -26,38 +42,109 @@ export default {
             ctx.fillStyle = color; //设置线的颜色状态
             ctx.fill();
         },
-        drawLine(ctx, x1, y1, x2, y2, lineWidth, color) {
+        drawLine(ctx) {
+            var lineWidth = this.lineWidth,
+                color = this.getForeColor();
             ctx.beginPath();
-            ctx.moveTo(x1, y1); //设置起点状态
-            ctx.lineTo(x2, y2); //设置末端状态
+            for (let index = 0; index < this.passBy.length; index++) {
+                const begin = this.passBy[index],
+                    end = this.passBy[index + 1];
+                ctx.moveTo(begin.x, begin.y);
+                ctx.lineTo(end.x, end.y);
+                if (index == this.passBy.length - 2)
+                    break;
+            }
             ctx.lineWidth = lineWidth; //设置线宽状态
             ctx.strokeStyle = color; //设置线的颜色状态
             ctx.stroke(); //进行绘制
             ctx.closePath();
         },
-        onResize() {
+        reDraw() {
             var w = this.detail.style.position.w;
             var h = this.detail.style.position.h;
             var el = this.$refs.elCanvas;
             var ctx = el.getContext("2d");
             ctx.clearRect(0, 0, w, h);
-            var x1 = 0,
-                y1 = h / 2,
-                x2 = w,
-                y2 = h / 2;
-            var color = this.getForeColor();
-            var lineWidth = this.detail.style.lineWidth;            
-            if (lineWidth == undefined) {
-                lineWidth = 2;
-            } else if(typeof lineWidth == 'string') {
-                lineWidth = parseInt(lineWidth);
-            }
-            this.drawLine(ctx, x1, y1, x2 - this.FACTOR_H * lineWidth, y2, lineWidth, color);
-            this.drawArrow(ctx, x1, y1, x2, y2, lineWidth, color);
+            this.drawLine(ctx);
+            this.drawArrow(ctx, this.passBy[this.passBy.length - 1].x, this.passBy[this.passBy.length - 1].y, this.lineWidth, this.getForeColor());
+
+        },
+        onResize() {
+            this.reDraw();
+        },
+        aroowPassDown(pass, event, index) {
+            this.flag = true;
+            pass.startX = event.pageX;
+            pass.startY = event.pageY;
+            pass.temp = {};
+            pass.temp.x = pass.x;
+            pass.temp.y = pass.y;
+            this.passItem = pass;
+        },
+        onMousemove(event) {
+            if (!this.flag)
+                return;
+            event.cancelBubble = true;
+            var dx = event.pageX - this.passItem.startX,
+                dy = event.pageY - this.passItem.startY;
+            this.passItem.x = this.passItem.temp.x + dx;
+            this.passItem.y = this.passItem.temp.y + dy;
+            this.reDraw();
+        },
+        onMouseUp(event) {
+            this.flag = false;
         }
     },
     mounted() {
+        var w = this.detail.style.position.w;
+        var h = this.detail.style.position.h;
+        var x1 = 0,
+            y1 = h / 2,
+            x2 = w,
+            y2 = h / 2;
+        var lineWidth = this.detail.style.lineWidth;
+        if (lineWidth == undefined) {
+            lineWidth = 2;
+        } else if (typeof lineWidth == 'string') {
+            lineWidth = parseInt(lineWidth);
+        }
+        this.passBy = [];
+        this.passBy.push({
+            x: x1,
+            y: y1
+        });
+        this.passBy.push({
+            x: 10,
+            y: 10
+        });
+        this.passBy.push({
+            x: 30,
+            y: 30
+        });
+        this.passBy.push({
+            x: x2 - this.FACTOR_H * lineWidth,
+            y: y2
+        });
         this.onResize();
     }
 }
 </script>
+
+<style lang="scss">
+.view-line-arrow {
+    height: 100%;
+    width: 100%;
+    position: relative;
+
+    .passby {
+        position: absolute;
+        height: 10px;
+        width: 10px;
+        background-color: white;
+        border: 1px solid #0cf;
+        left: -5px;
+        top: -5px;
+        cursor: move;
+    }
+}
+</style>
